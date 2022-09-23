2bbc8c46e8714a43e8c5a9e921ffe4dc5d79ef10
RBGonion <yopi27@naver.com>
2022-09-23 21:09:42 +0900

    fe101
    8~9강

--- 13.algorithm/practice.js
+++ 13.algorithm/practice.js
@@ -1,16 +1,9 @@
-function solution(k, arr) {
-  let answer = [];
-  for (let i = 0; i < arr.length; i++) {
-    let point = i;
-    let total = 0;
-    if (point === arr.length - 2) break;
-    while (point <= i + 2) {
-      total += arr[point++];
-    }
-    answer.push(total);
-  }
-  return Math.max(...answer);
+function solution(s) {
+  let answer;
+
+  answer;
+  return answer;
 }
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-console.log(solution(3, a));
+let str = "BACBACCACCBDEDE";
+console.log(solution(str));
