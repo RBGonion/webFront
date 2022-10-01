facf24b1b8db63b855994d2b1a0481b7721b79a3
RBGonion <yopi27@naver.com>
2022-10-02 01:38:39 +0900

    제로초 노드교과서
    1강 ~ 3-5강 예제

---
+++ 19.nodeCouseBook/lecture3/index.js
@@ -0,0 +1,11 @@
+const { odd, even } = require("./var");
+const checkNumber = require("./func");
+
+// console.log(checkNumber(10));
+// console.log(odd, even);
+
+// global.console.log(globalThis);
+
+for (let i = 9; i < 10; i--) {
+  console.log(i);
+}
