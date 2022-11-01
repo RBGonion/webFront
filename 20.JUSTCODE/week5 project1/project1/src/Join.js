605a928e9bccb641c22bbdaade6d45acba338b2b
RBGonion <yopi27@naver.com>
2022-11-01 19:51:00 +0900

    justcode week5
    (오류로 다시 커밋함2)
    아트워크 컴포넌트 레이아웃 구현
    카드리스트 컴포넌트 레이아웃 구현
    카드 컴포넌트 레이아웃 구현

--- 20.JUSTCODE/week5 project1/project1/src/Join.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ArtworkFilter() {
-  return <></>;
-}
-
-export default ArtworkFilter;
---
+++ 20.JUSTCODE/week5/project1/src/Join.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Join() {
+  return <></>;
+}
+
+export default Join;
