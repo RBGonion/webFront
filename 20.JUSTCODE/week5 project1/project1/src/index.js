2b6d08f59c53cb04d5a5125274f987567d8fc78c
RBGonion <yopi27@naver.com>
2022-11-01 19:25:16 +0900

    justcode week5
    (오류로 다시 커밋함)
    아트워크 컴포넌트 레이아웃 구현
    카드리스트 컴포넌트 레이아웃 구현
    카드 컴포넌트 레이아웃 구현

---
+++ 20.JUSTCODE/week5 project1/project1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Artwork from './Artwork';
+
+import './styles/reset.scss';
+import './styles/common.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Artwork />);
